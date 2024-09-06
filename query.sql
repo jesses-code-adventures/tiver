db5eea60e4912e6e32a85d81ab56188fc03f166e
jesse <jesse@rapidpaylegal.com>
2024-09-07 04:44:45 +1000

    feat: integrate queries into server

--- query.sql
+++ query.sql
@@ -1,30 +1,30 @@
---- name: CreateGame :one
+-- name: CreateGame :one
 INSERT INTO game DEFAULT VALUES RETURNING *;
 
---- name: UpdateEndGame :one
+-- name: UpdateEndGame :exec
 UPDATE game SET ended_at = $2 WHERE id = $1;
 
---- name: IncrementRequests :exec 
+-- name: IncrementRequests :exec 
 UPDATE game
 SET requests = requests + 1
 WHERE id = $1;
 
---- name: DecrementRequests :exec 
+-- name: DecrementRequests :exec 
 UPDATE game
 SET requests = requests + 1
 WHERE id = $1;
 
---- name: CreateRequest :one
+-- name: CreateRequest :one
 INSERT INTO request (id, created_at, game_id, colour, origin, speed, width, status) values (DEFAULT, DEFAULT, $1, $2, $3, $4, $5, $6) RETURNING *;
 
---- name: UpdateEndRequest :one
+-- name: UpdateEndRequest :exec
 UPDATE request SET status = $2, ended_at = $3 WHERE id = $1;
 
---- name: UpdateRequestStatus :exec 
+-- name: UpdateRequestStatus :exec 
 UPDATE request SET status = $2 WHERE id = $1;
 
---- name: GetActiveGames :many
+-- name: GetActiveGames :many
 SELECT * FROM game where ended_at = NULL order by created_at asc;
 
---- name: GetHangingRequests :manu 
+-- name: GetHangingRequests :many 
 SELECT * FROM game WHERE ended_at IS NOT NULL AND requests > 0;
